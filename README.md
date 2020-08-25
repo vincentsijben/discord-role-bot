ca1680a639e99c36d617a090e3cad30e19c89293
Vincent Sijben <vincent.sijben@zuyd.nl>
2020-08-25 10:45:47 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# discord-role-bot
+This Discord Bot lets users apply for roles themselves, by clicking emoticons corresponding to specific roles.
+It only works with reactions on the first message in a channel called "introduction". 
+Emoji names need to be the same as the corresponding roles, eg. Role "Immersive Design" and Emoji "immersivedesign". Spaces and capitals are ignored.
+
+To create the bot:
+```
+Go to https://discord.com/developers/applications
+Click New Application
+Give it a name like "CMD Role Bot"
+Give it a description
+Upload an app icon
+In the menu section go to Bot to add a Bot user
+Click Add Bot
+Copy the token and paste it in ./config/config.json
+In the menu section go to OAuth2
+In the scopes section choose "bot"
+In the bot permissions section choose "Manage Roles"
+Copy the URL, paste it in your browser, pick the correct server the bot needs to be added to.
+Go to Discord - Serversettings - Roles and make sure the bot role itself is above the other roles (right beneath admin)
+```
+
+
+
+To install this project:
+```
+npm install --save discordjs/discord.js
+sudo npm install -g nodemon
+```
+
+add this to the scripts section of package.json:
+```
+"dev": "nodemon ./src/bot.js"
+```
+and run the program with:
+```
+npm run dev
+```
+
+Now nodemon will start and every change in bot.js will restart the application automatically.
---
+++ src/bot.js
@@ -0,0 +1,110 @@
+const discord = require('discord.js');
+const client = new discord.Client({
+    partials: ['MESSAGE', 'REACTION']
+});
+const config = require('../config/config.json');
+client.login(config.TOKEN);
+
+client.on('ready', () => {
+    console.log(client.user.tag + " has logged in.");
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+
+    // only run code in introduction channel
+    if (!(reaction.message.channel.name === "introduction")) return;
+    console.log("this is the intro channel");
+
+    let firstMessage = await reaction.message.channel.messages.fetch({ after: 0, limit: 1 });
+    let firstMsgID = firstMessage.first().id;
+
+    let applyRole = async () => {
+        let emojiName = reaction.emoji.name;
+        let role = reaction.message.guild.roles.cache.find(role => role.name.toLowerCase().replace(/\s+/g, '') === emojiName.toLowerCase());
+        let member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+        try {
+            if (role && member) {
+                console.log("Role and member found.");
+                await member.roles.add(role);
+                console.log("Done adding role.");
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    //check if its a partial message (an old message, that was not cached)
+    if (reaction.message.partial) {
+        console.log("A partial.");
+        try {
+            let msg = await reaction.message.fetch();
+
+            // only run code if user reacts on the first message in this channel
+            if (msg.id === firstMsgID) applyRole();
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    else {
+        console.log("Not a partial.");
+
+        // only run code if user reacts on the first message in this channel
+        if (reaction.message.id === firstMsgID) applyRole();
+    }
+
+
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+
+    // only run code in introduction channel
+    if (!(reaction.message.channel.name === "introduction")) return;
+    console.log("this is the intro channel");
+
+    let firstMessage = await reaction.message.channel.messages.fetch({ after: 0, limit: 1 });
+    let firstMsgID = firstMessage.first().id;
+
+    let removeRole = async () => {
+        let emojiName = reaction.emoji.name;
+        let role = reaction.message.guild.roles.cache.find(role => role.name.toLowerCase().replace(/\s+/g, '') === emojiName.toLowerCase());
+        let member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+        try {
+            if (role && member) {
+                console.log("Role and member found.");
+                await member.roles.remove(role);
+                console.log("Done removing role.");
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    //check if its a partial message (an old message, that was not cached)
+    if (reaction.message.partial) {
+        console.log("A partial.");
+        try {
+            let msg = await reaction.message.fetch();
+
+            // only run code if user reacts on the first message in this channel
+            if (msg.id === firstMsgID) removeRole();
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    else {
+        console.log("Not a partial.");
+
+        // only run code if user reacts on the first message in this channel
+        if (reaction.message.id === firstMsgID) removeRole();
+    }
+
+
+})
+
+
+
+
